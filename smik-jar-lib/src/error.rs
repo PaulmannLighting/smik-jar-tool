e41c0a17a0e6acce6c3c60693ea0ce650e3838a3
Richard Neumann <neumann@paulmann.de>
2026-07-26 22:51:12 +0200

    Use `thiserror`.

--- smik-jar-lib/src/error.rs
+++ smik-jar-lib/src/error.rs
@@ -1,56 +1,21 @@
-use std::fmt::Display;
 use std::io;
 
 use java_properties::PropertiesError;
+use thiserror::Error;
 use zip::result::ZipError;
 
 /// Error type for the JAR library.
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum JarError {
     /// An I/O error occurred.
-    Io(io::Error),
+    #[error("I/O error: {0}")]
+    Io(#[from] io::Error),
 
     /// An error occurred while reading or writing the ZIP archive.
-    Zip(ZipError),
+    #[error("ZIP error: {0}")]
+    Zip(#[from] ZipError),
 
     /// An error occurred while parsing Java properties.
-    JavaProperties(PropertiesError),
-}
-
-impl Display for JarError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Io(error) => write!(f, "I/O error: {error}"),
-            Self::Zip(error) => write!(f, "ZIP error: {error}"),
-            Self::JavaProperties(error) => write!(f, "Error parsing Java properties: {error}"),
-        }
-    }
-}
-
-impl std::error::Error for JarError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Self::Io(error) => Some(error),
-            Self::Zip(error) => Some(error),
-            Self::JavaProperties(error) => Some(error),
-        }
-    }
-}
-
-impl From<io::Error> for JarError {
-    fn from(error: io::Error) -> Self {
-        Self::Io(error)
-    }
-}
-
-impl From<ZipError> for JarError {
-    fn from(error: ZipError) -> Self {
-        Self::Zip(error)
-    }
-}
-
-impl From<PropertiesError> for JarError {
-    fn from(error: PropertiesError) -> Self {
-        Self::JavaProperties(error)
-    }
+    #[error("Error parsing Java properties: {0}")]
+    JavaProperties(#[from] PropertiesError),
 }
